dccdd05130991f321735069392dca8f678416757
charulp2499 <charulpatel2499@gmail.com>
2023-05-20 12:37:48 +0530

    cmt

--- Manually_Draw_Box.py
+++ Manually_Draw_Box.py
@@ -1,12 +1,16 @@
 import os
 import cv2
 import csv
+import xml.etree.ElementTree as ET
+
 
 # Folder path containing the images
 folder_path = 'E:/Projects/Palm Print Detection/1/L'
 
 # Get a list of image files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
+
+annotation_root = ET.Element('annotations')
 
 # List to store the annotations
 annotations = []
@@ -37,6 +41,36 @@ for image_file in image_files:
     
     annotations.append({'image': image_file, 'bboxes': bboxes, 'labels': labels})
 
+    # Create the annotation entry for the image
+    annotation_element = ET.SubElement(annotation_root, 'annotation')
+    filename_element = ET.SubElement(annotation_element, 'filename')
+    filename_element.text = image_file
+    
+
+    # Iterate over the bounding boxes
+    for bbox, label in zip(bboxes, labels):
+        xmin, ymin, width, height = bbox
+        xmax = xmin + width
+        ymax = ymin + height
+
+        object_element = ET.SubElement(annotation_element, 'object')
+        name_element = ET.SubElement(object_element, 'name')
+        name_element.text = label
+        bndbox_element = ET.SubElement(object_element, 'bndbox')
+        xmin_element = ET.SubElement(bndbox_element, 'xmin')
+        xmin_element.text = str(xmin)
+        ymin_element = ET.SubElement(bndbox_element, 'ymin')
+        ymin_element.text = str(ymin)
+        xmax_element = ET.SubElement(bndbox_element, 'xmax')
+        xmax_element.text = str(xmax)
+        ymax_element = ET.SubElement(bndbox_element, 'ymax')
+        ymax_element.text = str(ymax)
+
+# Create the XML tree and save to file
+xml_tree = ET.ElementTree(annotation_root)
+xml_file_path = os.path.join(folder_path, 'annotations.xml')
+xml_tree.write(xml_file_path)
+
 # Save the annotations in a CSV file
 csv_file_path = os.path.join(folder_path, 'annotations.csv')
 with open(csv_file_path, 'w', newline='') as csv_file:
@@ -50,4 +84,4 @@ with open(csv_file_path, 'w', newline='') as csv_file:
             x, y, w, h = bbox
             writer.writerow([image_file, x, y, w, h, label])
 
-print('Annotations saved in annotations.csv')
+print('Annotations saved in annotations csv and XML')
