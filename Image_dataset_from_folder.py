96d1c5f561cf5f3ad62e1144a2d611f3dcbaed73
charulp2499 <charulpatel2499@gmail.com>
2023-05-20 15:18:36 +0530

    cmt

--- Image_dataset_from_folder.py
+++ Image_dataset_from_folder.py
@@ -1,45 +1,6 @@
-# import os
-# import cv2
-
-# # Define the paths
-# main_folder = "E:/Projects/Palm Print Detection/Database/"
-# output_folder = "E:/Projects/Palm Print Detection/Output"
-# print(os.walk(main_folder))
-# # Iterate through the main folder
-# for root, dirs, files in os.walk(main_folder):
-#     for folder in dirs:
-#         folder_path = os.path.join(root, folder)
-#         output_subfolder = os.path.join(output_folder, folder)
-
-#         # Create the output subfolder
-#         os.makedirs(output_subfolder, exist_ok=True)
-
-#         # Iterate through files in the subfolder
-#         for file in os.listdir(folder_path):
-#             file_path = os.path.join(folder_path, file)
-#             if file.endswith(".avi"):
-#                 # Open the video file
-#                 cap = cv2.VideoCapture(file_path)
-#                 frame_count = 30
-
-#                 # Iterate through frames
-#                 while cap.isOpened():
-#                     ret, frame = cap.read()
-#                     if not ret:
-#                         break
-
-#                     # Save each frame as an image
-#                     output_file = os.path.join(output_subfolder, f"{file}_{frame_count}.jpg")
-#                     cv2.imwrite(output_file, frame)
-
-#                     frame_count += 1
-
-#                 cap.release()
-
-# print("Image dataset creation complete.")
-
 import os
 import cv2
+import numpy as np
 
 def process_videos(root_folder, output_folder):
     for root, dirs, files in os.walk(root_folder):
@@ -68,14 +29,19 @@ def process_videos(root_folder, output_folder):
 
                         # Extract one frame per second
                         if frame_number % frame_rate  == 0:
+
                             # Resize the frame to a scaled size
-                            scale_percent = 40  # Adjust the scale as needed
+                            scale_percent = 40 
                             width = int(frame.shape[1] * scale_percent / 100)
                             height = int(frame.shape[0] * scale_percent / 100)
                             resized_frame = cv2.resize(frame, (width, height))
+                        
+                            # Apply sharpening filter to the resized frame
+                            kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+                            sharpened_frame = cv2.filter2D(resized_frame, -1, kernel)
 
                             output_file = os.path.join(output_subfolder, f"{file}_{count}.jpg")
-                            cv2.imwrite(output_file, resized_frame)
+                            cv2.imwrite(output_file, sharpened_frame)
                             count += 1
                         
                         frame_number += 1
@@ -87,7 +53,7 @@ def process_videos(root_folder, output_folder):
 # Define the paths
 
 root_folder = "E:/Projects/Palm Print Detection/Database/"
-output_folder = "E:/Projects/Palm Print Detection/Database_Image_Resize"
+output_folder = "E:/Projects/Palm Print Detection/Preprocessed_Dataset"
 
 process_videos(root_folder, output_folder)
 
